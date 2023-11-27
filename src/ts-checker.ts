9a356b10b73b48da9368535fdd8704dc7267fa4e
Dan Vanderkam <danvdk@gmail.com>
2023-11-27 15:19:16 -0500

    push absolute paths

--- src/ts-checker.ts
+++ src/ts-checker.ts
@@ -608,9 +608,11 @@ async function uncachedCheckTs(
   const fileName = sample.targetFilename || id + (sample.isTSX ? '.tsx' : `.${sample.language}`);
   const tsFile = writeTempFile(fileName, content);
   const sampleDir = getTempDir();
+  const allFiles = [tsFile];
   for (const auxFile of sample.auxiliaryFiles) {
     const {filename, content} = auxFile;
-    writeTempFile(filename, content);
+    const absPathFile = writeTempFile(filename, content);
+    allFiles.push(absPathFile);
   }
 
   const options: ts.CompilerOptions = {
@@ -623,7 +625,7 @@ async function uncachedCheckTs(
     options.typeRoots = [path.join(sampleDir, 'node_modules', '@types')];
   }
 
-  const program = ts.createProgram([tsFile], options, config.host);
+  const program = ts.createProgram(allFiles, options, config.host);
   const source = program.getSourceFile(tsFile);
   if (!source) {
     fail('Unable to load TS source file');

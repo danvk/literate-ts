25a3af81cab1de79865e17c22c49c2e71c75ecbb
Dan Vanderkam <danvdk@gmail.com>
2023-12-03 14:38:48 -0500

    snapshot missing assertion

--- src/test/asciidoc.test.ts
+++ src/test/asciidoc.test.ts
@@ -471,6 +471,7 @@ describe('checker', () => {
     './src/test/inputs/prepend-as-file.asciidoc',
     './src/test/inputs/check-jsonc.asciidoc',
     './src/test/inputs/node-output.asciidoc',
+    './src/test/inputs/twoslash-assertion.asciidoc',
   ])(
     'asciidoc checker snapshots %p',
     async inputFile => {

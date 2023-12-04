bb3b7c0c16fbb80aa136af2368c8a33f5a0163f0
Dan Vanderkam <danvdk@gmail.com>
2023-12-04 15:14:17 -0500

    a significant non-reproduction; maybe something to do w/ my node copying?

---
+++ src/repro.ts
@@ -0,0 +1,70 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+import ts from 'typescript';
+
+const unParsedConfig = ts.readConfigFile('tsconfig.json', ts.sys.readFile).config || {};
+const {options: tsOptions} = ts.parseJsonConfigFileContent(unParsedConfig, ts.sys, process.cwd());
+
+console.log('Verifying with TypeScript', ts.version);
+console.log('options', tsOptions);
+
+const host = ts.createCompilerHost(tsOptions, true);
+const srcPath = path.resolve('src/express.ts');
+
+const program = ts.createProgram([srcPath], tsOptions, host);
+const source = program.getSourceFile(srcPath);
+if (!source) {
+  throw new Error('Unable to recover source');
+}
+
+const diagnostics = ts.getPreEmitDiagnostics(program);
+console.log('diagnostics', diagnostics);
+
+// See https://github.com/JoshuaKGoldberg/eslint-plugin-expect-type/blob/a55413/src/rules/expect.ts#L506-L521
+export function getLanguageServiceHost(program: ts.Program): ts.LanguageServiceHost {
+  return {
+    getCompilationSettings: () => program.getCompilerOptions(),
+    getCurrentDirectory: () => program.getCurrentDirectory(),
+    getDefaultLibFileName: options => ts.getDefaultLibFilePath(options),
+    getScriptFileNames: () => program.getSourceFiles().map(sourceFile => sourceFile.fileName),
+    getScriptSnapshot: name =>
+      ts.ScriptSnapshot.fromString(program.getSourceFile(name)?.text ?? ''),
+    getScriptVersion: () => '1',
+    // NB: We can't check `program` for files, it won't contain valid files like package.json
+    fileExists: ts.sys.fileExists,
+    readFile: ts.sys.readFile,
+    readDirectory: ts.sys.readDirectory,
+    directoryExists: ts.sys.directoryExists,
+    getDirectories: ts.sys.getDirectories,
+  };
+}
+
+const checker = program.getTypeChecker();
+const languageService = ts.createLanguageService(getLanguageServiceHost(program));
+
+function getNodeAtPosition(sourceFile: ts.SourceFile, position: number): ts.Node | undefined {
+  let candidate: ts.Node | undefined = undefined;
+  ts.forEachChild(sourceFile, function iterate(node) {
+    const start = node.getStart();
+    const end = node.getEnd();
+    if (position >= start && position <= end) {
+      candidate = node;
+      ts.forEachChild(node, iterate);
+    }
+  });
+  return candidate;
+}
+
+const position = source.getPositionOfLineAndCharacter(12, 4);
+const node = getNodeAtPosition(source, position);
+if (!node) {
+  throw new Error(`Unable to find matching node at ${position}`);
+}
+console.log('matched node:', node.getText());
+const qi = languageService.getQuickInfoAtPosition(source.fileName, node.getStart());
+if (!qi?.displayParts) {
+  throw new Error(`Unable to get quickinfo for twoslash assertion`);
+}
+const actual = qi.displayParts.map(dp => dp.text).join('');
+console.log('type is', actual);

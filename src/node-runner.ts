f734b358415ec4ad63ed8beab4834faeacd1df1b
Dan Vanderkam <danvdk@gmail.com>
2024-03-11 14:01:58 -0400

    fix: support Node.js 20+, add launch.json for visual debugging (#256)
    Fixes #236 
    Fixes #245 
    
    Following
    https://medium.com/@mtiller/debugging-with-typescript-jest-ts-jest-and-visual-studio-code-ef9ca8644132
    The colorful stack traces in Node.js 20 were due to Jest setting
    `FORCE_COLOR=1`: https://github.com/jestjs/jest/issues/14391

--- src/node-runner.ts
+++ src/node-runner.ts
@@ -14,7 +14,15 @@ export interface ExecErrorType {
 /** Run a JavaScript program through Node.js. Returns the output. */
 export async function runNode(path: string): Promise<ExecErrorType> {
   try {
-    const {stdout, stderr} = await util.promisify(exec)(`node ${path}`);
+    const env = {
+      ...process.env,
+      FORCE_COLOR: '0', // See https://github.com/jestjs/jest/issues/14391
+      NODE_DISABLE_COLORS: '1', // Avoid colorful stack traces
+      NODE_OPTIONS: '', // Clear any potential debug options from launch.json
+    };
+    const {stdout, stderr} = await util.promisify(exec)(`node ${path}`, {
+      env,
+    });
     return {code: 0, stderr, stdout, path};
   } catch (eIn) {
     const e = eIn as ExecErrorType;

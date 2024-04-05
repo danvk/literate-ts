6e85694a69de55c3a93f74f8eab343bd84d01859
Dan Vanderkam <danvdk@gmail.com>
2024-04-05 14:22:12 -0400

    consolidate markdown/yaml extract tests

--- src/test/extract-sample.e2e.test.ts
+++ src/test/extract-sample.e2e.test.ts
@@ -6,20 +6,17 @@ import yaml from 'js-yaml';
 import {extractSamples} from '../code-sample.js';
 
 const TEST_DIR = 'src/test/inputs';
-const asciidocFiles = fs
+const inputFiles = fs
   .readdirSync(TEST_DIR, {recursive: true})
   .filter((path): path is string => typeof path === 'string')
-  .filter(path => path.endsWith('.asciidoc'))
+  .filter(path => path.endsWith('.md') || path.endsWith('.asciidoc'))
   .map(p => path.join(TEST_DIR, p));
 
-const markdownFiles = fs
-  .readdirSync(TEST_DIR, {recursive: true})
-  .filter((path): path is string => typeof path === 'string')
-  .filter(path => path.endsWith('.md'))
-  .map(p => path.join(TEST_DIR, p));
+// To update just this file's snapshots, run:
+// yarn baselines:update src/test/extract-sample.e2e.test.ts -t 'extract'
 
-describe('asciidoc extract baselines', () => {
-  it.each(asciidocFiles.map(file => [file]))('snapshot: %s', async inputFile => {
+describe('extract baselines', () => {
+  it.each(inputFiles.map(file => [file]))('snapshot: %s', async inputFile => {
     if (inputFile.includes('duplicate')) {
       return; // this one throws and is tested separately in asciidoc.test.ts
     }
@@ -27,22 +24,7 @@ describe('asciidoc extract baselines', () => {
     const content = await readFile(inputFile, 'utf8');
     const samples = extractSamples(content, name, base);
     const sampleTxt = yaml.dump(samples, {noRefs: true});
-    const baselineFile = `src/test/baselines/${name}.extract.yaml`;
-    if (process.env.UPDATE_MODE) {
-      await writeFile(baselineFile, sampleTxt, 'utf-8');
-    }
-    const expected = await readFile(baselineFile, 'utf-8');
-    expect(sampleTxt).toEqual(expected);
-  });
-});
-
-describe('markdown extract baselines', () => {
-  it.each(markdownFiles.map(file => [file]))('snapshot: %s', async inputFile => {
-    const {base, name} = path.parse(inputFile);
-    const content = await readFile(inputFile, 'utf8');
-    const samples = extractSamples(content, name, base);
-    const sampleTxt = yaml.dump(samples, {noRefs: true});
-    const baselineFile = `src/test/baselines/${name}.md.extract.yaml`;
+    const baselineFile = `src/test/baselines/${base}.extract.yaml`;
     if (process.env.UPDATE_MODE) {
       await writeFile(baselineFile, sampleTxt, 'utf-8');
     }

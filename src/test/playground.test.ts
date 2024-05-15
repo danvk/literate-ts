f9ab2934339e51162c98f9fe29b44eb9df495db2
Dan Vanderkam <danvdk@gmail.com>
2024-05-15 09:47:21 -0400

    fix playground test

--- src/test/playground.test.ts
+++ src/test/playground.test.ts
@@ -4,7 +4,7 @@ import {getPlaygroundUrl} from '../playground.js';
 describe('getPlaygroundUrl', () => {
   it('should encode a playground URL for a code snippet', () => {
     expect(getPlaygroundUrl(`console.log(2+2);`, {}, '5.3.3')).toEqual(
-      'https://www.typescriptlang.org/play/?ts=5.3.3#code/MYewdgziA2CmB00QHMAUAmA1OglAbiA=',
+      'https://www.typescriptlang.org/play/?ts=5.3.3#code/MYewdgziA2CmB00QHMAUAmA1OglAbiA',
     );
   });
 
@@ -16,7 +16,7 @@ describe('getPlaygroundUrl', () => {
         '5.5.0-beta',
       ),
     ).toEqual(
-      'https://www.typescriptlang.org/play/?ts=5.5.0-beta&strict=true&target=9#code/MYewdgziA2CmB00QHMAUAmA1OglAbiA=',
+      'https://www.typescriptlang.org/play/?ts=5.5.0-beta&strict=true&target=9#code/MYewdgziA2CmB00QHMAUAmA1OglAbiA',
     );
   });
 });
